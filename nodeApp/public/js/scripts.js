644e0ad5d45a3c5cadb54f21534e5a82d47ea586
David Hoang <dahoang@mail.sfsu.edu>
2017-10-23 00:11:19 +0000

    Reupload scripts.js

--- nodeApp/public/js/scripts.js
+++ nodeApp/public/js/scripts.js
@@ -106,3 +106,39 @@ function searchUser(){
 
     });
 }
+
+function searchListing(){
+    var listing = $('#listingSearch').val();
+    console.log(listing);
+
+    $.ajax({
+
+        url:"/searchListings/" + listing,
+        type:"post",
+        data:$("#search_form").serialize(),
+        success:function(res){
+
+            console.log(res, 'response');
+
+
+            var html = res;
+            $('#divResults').html(html);
+            //window.location.reload();
+
+            return false;
+        },
+        error:function(xhr, status, error){
+
+            console.log(error);
+            // console.log(xhr.responseText);
+            // var err = '';
+            // $.each(JSON.parse(xhr.responseText) , function(i, item) {
+            //
+            //     err +='<li>'+item.msg+'</li>';
+            // });
+            // $(".err-area").html(err);
+            // return false;
+        }
+
+    });
+}
